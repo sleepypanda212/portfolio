ba8dcb2fc509d9894f1b99e6c1e9116d60ad644c
sleepypanda212 <115032332+sleepypanda212@users.noreply.github.com>
2022-12-14 21:07:57 +0900

    final edit

--- app.js
+++ app.js
@@ -1,52 +1,112 @@
-        
-window.onload = function() {
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("header nav a");
 
-    var slider1 = new Slider({
-        images: '.slider-1 img',
-        btnPrev: '.slider-1 .buttons .prev',
-        btnNext: '.slider-1 .buttons .next',
-        auto: false
-    });
+/*Scrolling effect*/
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
 }
 
-function Slider(obj) {
+function reveal1() {
+  var reveals = document.querySelectorAll(".reveal1");
 
-	this.images = document.querySelectorAll(obj.images);
-	this.auto = obj.auto;
-	this.btnPrev = obj.btnPrev;
-	this.btnNext = obj.btnNext;
-     this.rate = obj.rate || 1000;
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-	var i = 0;
-     var slider = this;
-
-	this.prev = function () {
-		slider.images[i].classList.remove('shown');
-		i--;
-
-		if (i < 0) {
-			i = slider.images.length - 1;
-		}
-
-		slider.images[i].classList.add('shown');
-	}
-
-	this.next = function () {
-		slider.images[i].classList.remove('shown');
-		i++;
-
-		if (i >= slider.images.length) {
-			i = 0;
-		}
-
-		slider.images[i].classList.add('shown');
-	}
-
-    document.querySelector(slider.btnPrev).onclick = slider.prev;
-    document.querySelector(slider.btnNext).onclick = slider.next;
-
-	if (slider.auto)	{
-        setInterval(slider.next, slider.rate);
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
-};
-    
+  }
+}
+
+window.addEventListener("scroll", reveal1);
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+reveal1();
+/*Scrolling effect end*/
+
+/*Underline effect*/
+// VARIABLES
+const magicalUnderlines = Array.from(document.querySelectorAll('.underline--magical'));
+
+const gradientAPI = 'https://gist.githubusercontent.com/wking-io/3e116c0e5675c8bcad8b5a6dc6ca5344/raw/4e783ce3ad0bcd98811c6531e40256b8feeb8fc8/gradient.json';
+
+// HELPER FUNCTIONS
+
+// 1. Get random number in range. Used to get random index from array.
+const randNumInRange = max => Math.floor(Math.random() * (max - 1));
+
+// 2. Merge two separate array values at the same index to 
+// be the same value in new array.
+const mergeArrays = (arrOne, arrTwo) => arrOne
+  .map((item, i) => `${item} ${arrTwo[i]}`)
+  .join(', ');
+
+// 3. Curried function to add a background to array of elms
+const addBackground = (elms) => (color) => {
+  elms.forEach(el => {
+    el.style.backgroundImage = color;
+  });
+}
+// 4. Function to get data from API
+const getData = async(url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.data;
+}
+
+// 5. Partial Application of addBackground to always apply 
+// background to the magicalUnderlines constant
+const addBackgroundToUnderlines = addBackground(magicalUnderlines);
+
+// GRADIENT FUNCTIONS
+
+// 1. Build CSS formatted linear-gradient from API data
+const buildGradient = (obj) => `linear-gradient(${obj.direction}, ${mergeArrays(obj.colors, obj.positions)})`;
+
+// 2. Get single gradient from data pulled in array and
+// apply single gradient to a callback function
+const applyGradient = async(url, callback) => {
+  const data = await getData(url);
+  const gradient = buildGradient(data[randNumInRange(data.length)]);
+  callback(gradient);
+}
+
+// RESULT
+applyGradient(gradientAPI, addBackgroundToUnderlines);
+/*Underline effect end*/
